822832f00bfb52719d088676b71de4bf5b5f6a69
abdul dakkak <abduld@wolfram.com>
2017-06-05 10:08:09 +0100

    Update valid_os.go

--- pipeline/build/valid_os.go
+++ pipeline/build/valid_os.go
@@ -15,8 +15,8 @@ var valids = []string{
 	"linuxamd64",
 	"linuxarm",
 	"linuxarm64",
-	// "linuxppc64", - https://github.com/golang/go/issues/10087
-	// "linuxppc64le", - https://github.com/golang/go/issues/10087
+	"linuxppc64", 
+	"linuxppc64le", 
 	"linuxmips",
 	"linuxmipsle",
 	"linuxmips64",
